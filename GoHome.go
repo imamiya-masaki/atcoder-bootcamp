079a699d0cde74a547dbbbc9a87919e42407266f
imamiya-masaki <imamiyagit@gmail.com>
2021-03-10 22:50:49 +0900

    goHome

---
+++ GoHome.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scan_init()
+	x := int64(scanInt())
+	for i:=int64(0); i<=x; i++ {
+		left := i
+		right := left/2
+		if left %2 != 0 {
+			right++
+			if x <= left*right {
+				fmt.Println(left)
+				return
+			}
+		} else {
+			if x <= left*right + (left/2) {
+				fmt.Println(left)
+				return
+			}
+		}
+		// fmt.Println(left*right)
+	}
+
+}
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scan_init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+func scanInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+func scanInts(n int) []int {
+	take := make([]int, n)
+	for i := 0; i < n; i++ {
+		take[i] = scanInt()
+	}
+	return take
+}
+func scan() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+var rdr = bufio.NewReaderSize(os.Stdin, 200000)
+
+func readLine() string {
+	buf := make([]byte, 0, 200000)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+func readLineToNumber() int {
+	S := readLine()
+	number, _ := strconv.Atoi(S)
+	return number
+}
+func readLineToSlice() []string {
+	S := readLine()
+	list := strings.Split(S, "")
+	return list
+}
+func readLineToNumberSlice(memo map[string]int) []int {
+	// err時は-1を代入
+	S := readLine()
+	intList := make([]int, len(S))
+	for i := 0; i < len(S); i++ {
+		if val, ok := memo[string(S[i])]; ok {
+			intList[i] = val
+		} else {
+			intList[i] = -1
+		}
+	}
+	return intList
+}
